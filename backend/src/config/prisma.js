47f314148098062e36b2b7207ec84d6f264d9755
Hafizh Kusuma Abdillah <hafizh.abdillah@mhs.unsoed.ac.id>
2026-08-26 00:19:38 +0700

    reworked prisma.js

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,18 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-const adapter = new PrismaPg(pool);
+console.log('[Debug prisma] Initializing PrismaClient...');
 
-const prisma = new PrismaClient({
-  adapter,
-  log:
-    process.env.NODE_ENV === 'development'
-      ? ['query', 'error', 'warn']
-      : ['error'],
-});
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 module.exports = prisma;
