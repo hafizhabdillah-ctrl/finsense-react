db43bad393f324186e4860ad36248c1e071c9d47
Hafizh Kusuma Abdillah <hafizh.abdillah@mhs.unsoed.ac.id>
2026-08-25 23:55:32 +0700

    added console logs to authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
+console.log('[Debug authRoutes] 1. express loaded');
+
 const router = express.Router();
+
 const authController = require('../controllers/authController');
+console.log('[Debug authRoutes] 2. authController loaded');
+
 const {
   validateRegister,
   validateLogin,
@@ -8,8 +13,13 @@ const {
   validateRefreshToken,
   validateLogout,
 } = require('../utils/validation');
+console.log('[Debug authRoutes] 3. validation loaded');
+
 const validate = require('../middleware/validate');
+console.log('[Debug authRoutes] 4. validate loaded');
+
 const auth = require('../middleware/auth');
+console.log('[Debug authRoutes] 5. auth middleware loaded');
 
 router.post('/register', validate(validateRegister), authController.register);
 router.post('/login', validate(validateLogin), authController.login);
