128cbf85c863e49e180d7f15ac6bcb547e380972
Hafizh Kusuma Abdillah <hafizh.abdillah@mhs.unsoed.ac.id>
2026-08-26 00:47:07 +0700

    updated prisma.js

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,16 +1,17 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
 
 let prismaInstance;
 
 function getPrismaClient() {
   if (!prismaInstance) {
-    console.log('[Debug prisma] Creating PrismaClient instance...');
-    prismaInstance = new PrismaClient({
-      log:
-        process.env.NODE_ENV === 'development'
-          ? ['query', 'error', 'warn']
-          : ['error'],
+    console.log('[Debug prisma] Creating PrismaClient instance with pg adapter...');
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
     });
+    const adapter = new PrismaPg(pool);
+    prismaInstance = new PrismaClient({ adapter });
   }
   return prismaInstance;
 }
