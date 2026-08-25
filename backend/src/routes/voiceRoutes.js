5b1377e07e07b6dce542afbbd637d87b826383f9
Hafizh Kusuma Abdillah <hafizh.abdillah@mhs.unsoed.ac.id>
2026-08-25 22:52:58 +0700

    changed upload

--- backend/src/routes/voiceRoutes.js
+++ backend/src/routes/voiceRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const voiceController = require('../controllers/voiceController');
 
 const multer = require('multer');
-const upload = multer({ dest: '../uploads' });
+const upload = multer({ dest: '/tmp/uploads/' });
 
 router.post('/voice', upload.single('audio'), voiceController.processVoice);
 
